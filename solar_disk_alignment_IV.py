3546b638c5905c5e8103da0dc0770385d7ce8593
Dmitry <astronom.v.cube@gmail.com>
2024-05-08 14:57:16 +0300

    Перенос части кода в утилиты, добавление метода с центром тяжести

--- solar_disk_alignment_IV.py
+++ solar_disk_alignment_IV.py
@@ -9,20 +9,20 @@ import os, sys, re
 import shutil
 import logging
 from tqdm import tqdm
+from analise_utils import ArrayOperations, Extract, Monitoring, OsOperations
 
-def logprint(msg):
-    print(msg)
-    logging.info(msg)
-    
-logging.basicConfig(filename = 'logs.log',  filemode='a', level = logging.INFO, format = '%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+Monitoring.start_log('log')
+extract = Extract()
 logging.info(f'Start program alignment of the solar disk')
 # norm=TwoSlopeNorm(vmin=0, vcenter=vcenter) /// colors.Normalize(vmin=0, vcenter=vcenter))
 
 ##########
-a = '20220120T055430'
+a = '20220120T055630'
 directory = f"D:/datasets/20.01.22/times/{a}_calibrated_brightness"
 mode = 'saved_settings'     # 'saved_settings' or 'interactive'
-vcenter = 5000
+method = 'MAP' # 'MAP' /max around point/ or 'COM' /center of mass/
+postfix = 'MAP_aligned' if method == 'MAP' else 'COM_aligned'
+vcenter = 5000 
 # x_limit = (350, 450)
 # y_limit = (580, 680)
 
@@ -32,16 +32,8 @@ setting_of_alignes = []
 ##########
 logging.info(f'Path to files: {directory}')
 
-pattern = re.compile(r'(?<=[_.])\d{4,5}(?=[_.])')
 freqs = set()
-
-# функция для извлечения цифр из названия файла
-def extract_number(filename):
-    match = pattern.search(filename)
-    freqs.add(int(match.group()))
-    return int(match.group())
-
-files = sorted(os.listdir(directory), key=extract_number)
+files = sorted(os.listdir(directory), key=lambda x: extract.extract_number(x, freqs))
 freqs = sorted(list(freqs), reverse = True)
 iterable = range(0, len(files), 2) 
 
@@ -64,31 +56,6 @@ def update(val):
     im.set_clim(vmin, vmax)
     ax.figure.canvas.draw_idle()
 
-def find_max_around_point(matrix : np.ndarray, point : tuple, size : int):
-    """     
-    The function searches for the maximum value in the matrix within a certain size around a given point. 
-    The input arguments of the function are the matrix `matrix` (two-dimensional array), the specified point `point` (coordinates) and the size of the search area `size` (integer).
-    The function returns a tuple of three values: the row `max_row` and column `max_col` of the maximum value, as well as the value `max_value` of the largest element in the specified area.
-    """
-    # Get the indices of the smaller region around the point
-    row, col = point
-    half_size = size // 2
-    row_indices = range(max(0, row - half_size), min(matrix.shape[0], row + half_size + 1))
-    col_indices = range(max(0, col - half_size), min(matrix.shape[1], col + half_size + 1))
-    
-    # Get the smaller region from the original matrix
-    smaller_matrix = matrix[np.ix_(row_indices, col_indices)]
-    
-    # Find the maximum value in the smaller region
-    max_value = np.max(smaller_matrix)
-    
-    # Get the coordinates of the maximum value in the original matrix
-    max_indices = np.unravel_index(np.argmax(smaller_matrix), smaller_matrix.shape)
-    max_row = row_indices[max_indices[0]]
-    max_col = col_indices[max_indices[1]]
-    
-    return (max_row, max_col, max_value)
-
 if mode == 'saved_settings':
     setting_of_alignes = np.load('setting_of_alignes.npy')
 
@@ -97,7 +64,6 @@ elif mode == 'interactive':
     coordinates_of_control_point = []
     coordinates_of_max_point_in_area = []
     square_psf = list()
-
 
     for i in iterable:
         
@@ -144,13 +110,13 @@ elif mode == 'interactive':
 
         # Вывод последней координаты двойного клика пользователя
         if len(click_coords) > 0:
-            logprint(f"For image {i+1} last double click coordinates: {str(click_coords[-1])}")
+            Monitoring.logprint(f"For image {i+1} last double click coordinates: {str(click_coords[-1])}")
             coordinates_of_control_point.append(click_coords[-1])
             coordinates_of_control_point.append(click_coords[-1])
             plt.close()
             
         else:
-            logprint(f"No double click coordinates recorded")
+            Monitoring.logprint(f"No double click coordinates recorded")
             sys.exit()
 
 def alignment_sun_disk(files : list = files, method : str = 'search_max_in_area', area : int = None):
@@ -173,30 +139,27 @@ def alignment_sun_disk(files : list = files, method : str = 'search_max_in_area'
             if method == 'search_max_in_area':
 
                 if int(np.sqrt(img1.size)) == 1024:
-                    max_col, max_row, max_value = find_max_around_point(I, reversed(control_point_1), 5)
+                    max_col, max_row, max_value = ArrayOperations.find_max_around_point(I, reversed(control_point_1), 14) if method == 'MAP' else ArrayOperations.find_center_of_mass(I, reversed(control_point_1), 14)
 
                 elif int(np.sqrt(img1.size)) == 512:
-                    max_col, max_row, max_value = find_max_around_point(I, reversed(control_point_1), 13)
+                    max_col, max_row, max_value = ArrayOperations.find_max_around_point(I, reversed(control_point_1), 28) if method == 'MAP' else ArrayOperations.find_center_of_mass(I, reversed(control_point_1), 28)
                     
                 control_point_1 = (max_row, max_col)
                 coordinates_of_max_point_in_area.append(control_point_1)
                 logging.info(f"Max - {max_value} in {control_point_1}")
                 
         except Exception as err:
-            logprint('The program is terminated due to lack of alignment data')
-            logprint('err')
+            Monitoring.logprint('The program is terminated due to lack of alignment data')
+            Monitoring.logprint('err')
         
     # Определяем место для сохранения
-    try:
-        os.mkdir(f'{directory}_aligned')
-    except:
-        shutil.rmtree(f'{directory}_aligned')
-        os.mkdir(f'{directory}_aligned')
+    
+    OsOperations.create_place(directory, postfix)
 
     # пересохранение первой пары файлов в новую директорию
-    fits.writeto(f'{directory}_aligned/{files[0][:-4]}_aligned.fits', img1, overwrite=True, header=header1)
+    fits.writeto(f'{directory}_{postfix}/{files[0][:-4]}_{postfix}.fits', img1, overwrite=True, header=header1)
     logging.info(f"Image {1}: {files[0]} - saved")
-    fits.writeto(f'{directory}_aligned/{files[1][:-4]}_aligned.fits', img2, overwrite=True, header=header2)
+    fits.writeto(f'{directory}_{postfix}/{files[1][:-4]}_{postfix}.fits', img2, overwrite=True, header=header2)
     logging.info(f"Image {2}: {files[1]} - saved")
     
     hdul1.close()
@@ -221,16 +184,16 @@ def alignment_sun_disk(files : list = files, method : str = 'search_max_in_area'
             try:
                 control_point_2 = (coordinates_of_control_point[i][0], coordinates_of_control_point[i][1])  # координаты признака на текущем изображени
             except Exception as err:
-                logprint('The program is terminated due to lack of alignment data')
-                logprint(err)
+                Monitoring.logprint('The program is terminated due to lack of alignment data')
+                Monitoring.logprint(err)
 
             if method == 'search_max_in_area':
 
                 if int(np.sqrt(img2.size)) == 1024:
-                    max_col, max_row, max_value = find_max_around_point(I, reversed(control_point_2), 5)
+                    max_col, max_row, max_value = ArrayOperations.find_max_around_point(I, reversed(control_point_2), 14)
 
                 elif int(np.sqrt(img2.size)) == 512:
-                    max_col, max_row, max_value = find_max_around_point(I, reversed(control_point_2), 8)
+                    max_col, max_row, max_value = ArrayOperations.find_max_around_point(I, reversed(control_point_2), 28)
                     
                 control_point_2 = (max_row, max_col)
                 coordinates_of_max_point_in_area.append(control_point_2)
@@ -252,12 +215,12 @@ def alignment_sun_disk(files : list = files, method : str = 'search_max_in_area'
         img2 = np.roll(img2, dy, axis=0)
 
         # Сохранение выравненных изображений
-        fits.writeto(f'{directory}_aligned/{files[i][:-4]}_aligned.fits', img1, overwrite=True, header=header1)
+        fits.writeto(f'{directory}_{postfix}/{files[i][:-4]}_{postfix}.fits', img1, overwrite=True, header=header1)
         logging.info(f"Image {i+2}: {files[i]} - saved")   
-        fits.writeto(f'{directory}_aligned/{files[i+1][:-4]}_aligned.fits', img2, overwrite=True, header=header2)
+        fits.writeto(f'{directory}_{postfix}/{files[i+1][:-4]}_{postfix}.fits', img2, overwrite=True, header=header2)
         logging.info(f"Image {i+2}: {files[i+1]} - saved")      
         
-    logprint('Finish program alignment of the solar disk')
+    Monitoring.logprint('Finish program alignment of the solar disk')
     print('For more details: read file "logs.log"')
     
     if mode == 'interactive':
