6a55f6898d483c28dbd0f8e45fa083ced94a4e40
Dmitry <astronom.v.cube@gmail.com>
2024-08-08 19:52:31 +0300

    update docs script

--- docs/update_docs.py
+++ docs/update_docs.py
@@ -1,8 +1,9 @@
 import os
 
 os.system('cd ..')
+os.remove('docs/index.html')
 
-# os.system('python -m pdoc --html --output-dir doc --math --search -logo https://raw.githubusercontent.com/astronom-v-cube/SRH-analysis/main/docs/logo.jpg ../analise_utils.py')
-
-os.system('python -m pdoc --pdf --force --output-dir docs analise_utils.py')
+# os.system('python -m pdoc --pdf --force --output-dir docs analise_utils.py')
 os.system('python -m pdoc --html --force --output-dir docs analise_utils.py')
+
+os.rename('docs/analise_utils.html', 'docs/index.html')
