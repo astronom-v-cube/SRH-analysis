8be04f0070d3a28159dcc1b25eabb66278f53fb9
Dmitry <astronom.v.cube@gmail.com>
2024-08-08 20:10:06 +0300

    мелкие правки

--- docs/update_docs.py
+++ docs/update_docs.py
@@ -3,7 +3,6 @@ import os
 os.system('cd ..')
 os.remove('docs/index.html')
 
-# os.system('python -m pdoc --pdf --force --output-dir docs analise_utils.py')
 os.system('python -m pdoc --html --force --output-dir docs analise_utils.py')
 
 os.rename('docs/analise_utils.html', 'docs/index.html')
