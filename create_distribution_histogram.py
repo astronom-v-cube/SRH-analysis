04fc7b181f8554c0a9b0b692186be9e68ed021d0
Dmitry <astronom.v.cube@gmail.com>
2024-08-09 14:48:58 +0300

    gist

---
+++ create_distribution_histogram.py
@@ -0,0 +1,39 @@
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+
+from analise_utils import MplFunction, ArrayOperations, Monitoring
+MplFunction.set_mpl_rc()
+
+def create_distribution_histogram(input_file : str, replace_minus_to_zero = True):
+    """Построение гистограммы распределения значений яркостной температуры в ```.fits``` файле
+
+    Args:
+        input_file (str): путь к файлу
+        replace_minus_to_zero (bool, optional): параметр, регулирующий замену отрицательных значений на нуль при подсчете. По умолчанию установлен True.
+    """
+
+    fig = plt.figure(figsize = (25, 15))
+    fig.canvas.manager.window.showMaximized()
+
+    if replace_minus_to_zero:
+        data, header = ArrayOperations.replace_minus_to_zero(input_file)
+        plt.xlim(0, data.max())
+    else:
+        hdul = fits.open(input_file)
+        data = hdul[0].data
+        header = hdul[0].header
+        plt.xlim(data.min(), data.max())
+
+    Monitoring.header_info(header)
+    plt.hist(data.flatten(), bins='auto', edgecolor='black')
+    plt.title('Гистограмма распределения')
+    plt.xlabel('Значение яркостной температуры')
+    plt.ylabel('Количество пикселей')
+    plt.yscale("log")
+    plt.tight_layout()
+
+    plt.show()
+
+if __name__ == "__main__":
+    create_distribution_histogram('test_dataset/srh_20220113T031122_6200_LCP.fit')
